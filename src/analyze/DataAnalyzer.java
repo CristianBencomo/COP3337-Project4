bc3e3fca2e15e766c51c78bec86d5a93d4a82b7a
CristianBencomo <cristian.bencomo@outlook.com>
2020-07-15 15:22:43 -0400

    initial Commit

---
+++ src/analyze/DataAnalyzer.java
@@ -0,0 +1,34 @@
+package analyze;
+
+import app.ExecutionInfo;
+import java.util.ArrayList;
+
+public class DataAnalyzer {
+    
+    private ArrayList<ExecutionInfo> data; 
+
+    public DataAnalyzer() {
+        
+        data = new ArrayList<>();
+    }
+
+    public ArrayList<ExecutionInfo> getData() {
+        return data;
+    }
+    
+    public void findGrowthRate(){
+          System.out.println("");
+          
+         System.out.println("-------------------------------------------------");
+         System.out.println("Data Analyzer Growth Rate");
+         System.out.println("-------------------------------------------------");
+         
+         // your output format must match mine
+         // look at the video for see the out format
+         // the exponent value is e^x = total method class   
+         // you need to find the x... do not over think it use the Math class 
+         // YOU PUT YOUR CODE HERE
+
+    }
+    
+}// end class
