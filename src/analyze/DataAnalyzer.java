d304cf571a59c3b5af77ab2d121890b41cba989b
CristianBencomo <cristian.bencomo@outlook.com>
2020-07-25 22:13:52 -0400

    new commit

--- src/analyze/DataAnalyzer.java
+++ src/analyze/DataAnalyzer.java
@@ -23,11 +23,7 @@ public class DataAnalyzer {
          System.out.println("Data Analyzer Growth Rate");
          System.out.println("-------------------------------------------------");
          
-         // your output format must match mine
-         // look at the video for see the out format
-         // the exponent value is e^x = total method class   
-         // you need to find the x... do not over think it use the Math class 
-         // YOU PUT YOUR CODE HERE
+         
         for(int i=0; i < data.size(); i++) {
             double exponent = Math.log(data.get(i).getTotalMethodCalls());
             
